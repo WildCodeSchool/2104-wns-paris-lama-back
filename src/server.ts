f241264b8180fd85b8262d2c9894617a819c73aa
lawndlwd <lwdlwd95@gmail.com>
2021-04-28 10:22:24 +0200

    add type-graphql & implement CRUD for the course

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,38 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import cors from 'cors'
 import colors from 'colors'
+import { config } from 'dotenv'
+import { buildSchema } from 'type-graphql'
+import 'reflect-metadata'
+import { ApolloServer } from 'apollo-server-express'
+
 import connectDB from './config/db.config'
 
-connectDB()
-const app = express()
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors())
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello')
-})
+import CourseResolver from './resolvers/course.resolver'
+// eslint-disable-next-line prettier/prettier
+const startserver = async () => {
+  config()
+  connectDB()
+  const schema = await buildSchema({
+    resolvers: [CourseResolver],
+    emitSchemaFile: true,
+    validate: false,
+  })
 
-app.listen(8080, () => {
-  console.log(colors.bgWhite.black('Server start on Port 8080'))
-})
+  const server = new ApolloServer({ schema })
+  await server.start()
+
+  const app = express()
+  app.use(cors())
+
+  server.applyMiddleware({ app })
+
+  app.listen({ port: 8080 })
+  console.log(
+    colors.bgBlack.white(
+      `Server ready 🦙🦙🦙  at http://localhost:8080${server.graphqlPath}`
+    )
+  )
+  return { server, app }
+}
+startserver()
