65dd1661025ca49ff6a63034a9706cb3207a09ca
lawndlwd <lwdlwd95@gmail.com>
2021-06-23 14:04:13 +0200

    fix delete course

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -5,7 +5,7 @@ import colors from 'colors'
 
 const connectDB = async (): Promise<void> => {
   try {
-    const uri = process.env.DB || 'mongodb://127.0.0.1:27017/lama'
+    const uri = process.env.DB || 'mongodb://mongodb:27017/lama'
     const connect = await mongoose.connect(uri, {
       useCreateIndex: true,
       useUnifiedTopology: true,
