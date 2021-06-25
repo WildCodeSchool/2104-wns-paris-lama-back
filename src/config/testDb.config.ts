f1431e9a57ef74428a1e36c6aa3012445832ee6e
lawndlwd <lwdlwd95@gmail.com>
2021-06-25 15:54:06 +0200

    fix test

--- src/config/testDb.config.ts
+++ src/config/testDb.config.ts
@@ -1,6 +1,3 @@
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-console */
 import mongoose from 'mongoose'
 import colors from 'colors'
@@ -14,16 +11,12 @@ export const connectDBTEST = async (): Promise<void> => {
 
     const mongooseOpts = {
       useNewUrlParser: true,
-      autoReconnect: true,
-      reconnectTries: Number.MAX_VALUE,
-      reconnectInterval: 1000,
       useCreateIndex: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
     }
 
     await mongoose.connect(uri, mongooseOpts)
-    console.log(colors.bgGreen.black('DBTEST connected'))
   } catch (error) {
     console.log(colors.red(error))
     process.exit(1)
@@ -33,4 +26,10 @@ export const closeDatabase = async (): Promise<void> => {
   await mongoose.connection.dropDatabase()
   await mongoose.connection.close()
   await mongod.stop()
+}
+export const clearDatabase = async (): Promise<void> => {
+  const { collections } = mongoose.connection
+  Object.values(collections).forEach(async (collection: any) => {
+    await collection.deleteMany({})
+  })
 }
