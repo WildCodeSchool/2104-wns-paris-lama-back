dfa74bf0b295c55fdc78f0b9a5df164c4110c493
Levend <levend-mohammad_student2020@wilder.school>
2021-12-01 14:12:51 +0100

    F add auth (#20)
    * added port for mongo (#11)
    
    Co-authored-by: Axel Hmlcr <axel.hamilcaro@icloud.com>
    
    * Update README.md
    
    * Update README.md
    
    * merge
    
    * add tiptap
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    Co-authored-by: Axel Hamilcaro <68954111+axelhamil@users.noreply.github.com>
    Co-authored-by: Axel Hmlcr <axel.hamilcaro@icloud.com>
    Co-authored-by: Levende <levende.mohammad@gravity.earth>

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 echo "PORT=$PORT"
-git fetch && git reset --hard origin/main && git clean -f -d
-GATEWAY_PORT=$PORT BRANCH_NAME=staging docker-compose -f docker-compose.yml up --build -d
+git fetch && git reset --hard origin/main && git clean -f -d && GATEWAY_PORT=$PORT BRANCH_NAME=staging docker-compose -f docker-compose.yml up --build -d
